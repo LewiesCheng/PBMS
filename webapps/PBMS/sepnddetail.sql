19f31de5371d217d4563840917b6a6f4696f2fdd
JoyHwong <hzy961165750@qq.com>
2016-06-06 21:46:01 +0800

    database

---
+++ webapps/PBMS/sepnddetail.sql
@@ -0,0 +1,100 @@
+
+create table spenddetail(
+    y char(4) not null,
+    m char(4) not null,
+    food float,
+    rent float,
+    educate float,
+    utilitie float,
+    medical float,
+    primary key (y, m)
+);
+
+insert into spenddetail values(
+    '2016', '6', 500, 1034, 800, 345, 213
+);
+
+
+insert into spenddetail values(
+    '2016', '5', 492, 1024, 823, 391, 201
+);
+
+
+insert into spenddetail values(
+    '2016', '4', 501, 1012, 823, 391, 201
+);
+
+
+insert into spenddetail values(
+    '2016', '3', 495, 1832, 822, 413, 411
+);
+
+
+insert into spenddetail values(
+    '2016', '2', 502, 1024, 853, 421, 295
+);
+
+
+insert into spenddetail values(
+    '2016', '1', 614, 1051, 823, 311, 315
+);
+
+
+insert into spenddetail values(
+    '2015', '12', 543, 652, 653, 543, 653
+);
+
+
+insert into spenddetail values(
+    '2015', '11', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '10', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '9', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '8', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '7', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '6', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '5', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '4', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '3', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '2', 765, 1245, 876, 381, 301
+);
+
+
+insert into spenddetail values(
+    '2015', '1', 765, 1245, 876, 381, 301
+);
